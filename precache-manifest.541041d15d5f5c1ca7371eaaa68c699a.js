580adacaf4182c0c777e1602c0da049260a0f51e
Radhika Kshirsagar <radhika.kshirsagar@vimeo.com>
2023-07-28 10:20:17 -0700

    Updates

---
+++ precache-manifest.541041d15d5f5c1ca7371eaaa68c699a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "21593e074b94bab8ac0c",
+    "url": "/wavelength-game/static/css/main.525fdcf6.chunk.css"
+  },
+  {
+    "revision": "21593e074b94bab8ac0c",
+    "url": "/wavelength-game/static/js/main.1c207ff1.chunk.js"
+  },
+  {
+    "revision": "9b579097b285a752a455",
+    "url": "/wavelength-game/static/js/runtime~main.8d650d12.js"
+  },
+  {
+    "revision": "4115ae12ad75ac1cd9eb",
+    "url": "/wavelength-game/static/js/2.48bfc66a.chunk.js"
+  },
+  {
+    "revision": "f499b7737514e727b304",
+    "url": "/wavelength-game/static/js/3.58bb4b7d.chunk.js"
+  },
+  {
+    "revision": "83558054993df34a2ec6b3f819a7e918",
+    "url": "/wavelength-game/static/media/shuffle.83558054.png"
+  },
+  {
+    "revision": "5cab8ac623f1af130d978fbe21aed8a9",
+    "url": "/wavelength-game/static/media/redpointer.5cab8ac6.png"
+  },
+  {
+    "revision": "d58934fe7377dcd53e3e07dbfc05e71d",
+    "url": "/wavelength-game/static/media/show.d58934fe.png"
+  },
+  {
+    "revision": "e68fd803889384084270f7d71fea6e38",
+    "url": "/wavelength-game/static/media/hide.e68fd803.png"
+  },
+  {
+    "revision": "547210dd9b066f46bb3f127b3585560a",
+    "url": "/wavelength-game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/wavelength-game/precache-manifest.541041d15d5f5c1ca7371eaaa68c699a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/wavelength-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});

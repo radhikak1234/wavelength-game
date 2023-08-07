e21acef98fd301aecbc2f21330fce47b8170612c
Radhika Kshirsagar <radhika.kshirsagar@vimeo.com>
2023-08-07 00:16:06 -0700

    Updates

--- precache-manifest.5908ddbb9e2b4511bfa88a603a9d78b6.js
+++ precache-manifest.5908ddbb9e2b4511bfa88a603a9d78b6.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = [
   {
-    "revision": "da1b481edbf9bc972634",
+    "revision": "1af9caf5f7e402e105a5",
     "url": "/wavelength-game/static/css/main.525fdcf6.chunk.css"
   },
   {
-    "revision": "da1b481edbf9bc972634",
-    "url": "/wavelength-game/static/js/main.a1599210.chunk.js"
+    "revision": "1af9caf5f7e402e105a5",
+    "url": "/wavelength-game/static/js/main.82351177.chunk.js"
   },
   {
     "revision": "d740156408e92b326a3d",
     "url": "/wavelength-game/static/js/runtime~main.a24dc772.js"
   },
   {
-    "revision": "a614327a8c0c5d1946fa",
-    "url": "/wavelength-game/static/js/2.7098c4f7.chunk.js"
+    "revision": "5293e73e2acf6039e8d0",
+    "url": "/wavelength-game/static/js/2.c8eac745.chunk.js"
   },
   {
     "revision": "e897de3d059808493f9f",
     "url": "/wavelength-game/static/js/3.5aaf3fca.chunk.js"
   },
   {
-    "revision": "83558054993df34a2ec6b3f819a7e918",
-    "url": "/wavelength-game/static/media/shuffle.83558054.png"
-  },
-  {
     "revision": "5cab8ac623f1af130d978fbe21aed8a9",
     "url": "/wavelength-game/static/media/redpointer.5cab8ac6.png"
+  },
+  {
+    "revision": "83558054993df34a2ec6b3f819a7e918",
+    "url": "/wavelength-game/static/media/shuffle.83558054.png"
   },
   {
     "revision": "d58934fe7377dcd53e3e07dbfc05e71d",
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "/wavelength-game/static/media/hide.e68fd803.png"
   },
   {
-    "revision": "fcaed0023c18794278151a75ce4d3285",
+    "revision": "4ab914cb5e72299a741befe2e4ca8a5a",
     "url": "/wavelength-game/index.html"
   }
 ];

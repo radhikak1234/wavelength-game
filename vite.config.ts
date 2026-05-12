8f68ab78b1102e35b082a871fd6f09353d589938
Radhika Kshirsagar <radhikakshirsagar@Radhikas-MacBook-Pro.local>
2026-05-12 13:12:48 -0700

    Add automatic build step for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  base: "/wavelength-game/",
+export default defineConfig(() => ({
+  base: process.env.GITHUB_PAGES === "true" ? "/wavelength-game/" : "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/setupTests.ts",
   },
-});
+}));
